47624dff3657ca5b62f52e463945c1afa468cb50
jwhlaforest <jwh.laforest@gmail.com>
2018-08-10 14:31:52 -0500

    further data exploration with IIHS

--- .ipynb_checkpoints/config-checkpoint.py
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -11,9 +11,13 @@ owm_url = 'api.openweathermap.org/data/2.5/weather?'
 owm_key = '534aff4eb81c8a963f47e01c285118f7'
 
 # Google
-google_url = '"https://maps.googleapis.com/maps/api/'
+google_url = 'https://maps.googleapis.com/maps/api/'
 google_key = 'AIzaSyANwwy09uDhP8-gi5HCNJr_Ncv2Dic8tEo'
 
 # Census.gov
 gov_cen_url = 'https://api.census.gov/data/'
 gov_cen_key = '14fc1a5baaa9f9201beac022a10ae9003fbf76c3'
+
+#IIHS.gov
+iihs_url = 'https://api.iihs.org/v4/'
+iihs_key = 'gx0GXGV6tEqkzh4o6Wd5kXnCCxQOClVGgh-ewm8U_-8'
